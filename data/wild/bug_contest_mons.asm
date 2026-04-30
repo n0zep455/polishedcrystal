cc6c08d5796f811d083cf8ee5c0e3421c71d53e6
n0ze_ <117369253+n0zep455@users.noreply.github.com>
2026-04-30 12:57:24 -0400

    Restore normal contest mons

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -6,19 +6,19 @@ ENDM
 
 ; Base Bug Catching Contest
 ContestMons:
-	;            %, species,     min,                    max
-	contest_mon 15, CATERPIE,    LEVEL_FROM_BADGES - 5,  LEVEL_FROM_BADGES + 6
-	contest_mon 15, WEEDLE,      LEVEL_FROM_BADGES - 5,  LEVEL_FROM_BADGES + 6
-	contest_mon 10, METAPOD,     LEVEL_FROM_BADGES - 3,  LEVEL_FROM_BADGES + 6
-	contest_mon 10, KAKUNA,      LEVEL_FROM_BADGES - 3,  LEVEL_FROM_BADGES + 6
-	contest_mon  5, BUTTERFREE,  LEVEL_FROM_BADGES + 0,  LEVEL_FROM_BADGES + 3
-	contest_mon  5, BEEDRILL,    LEVEL_FROM_BADGES + 0,  LEVEL_FROM_BADGES + 3
-	contest_mon 10, VENONAT,     LEVEL_FROM_BADGES - 2,  LEVEL_FROM_BADGES + 4
-	contest_mon 10, PARAS,       LEVEL_FROM_BADGES - 2,  LEVEL_FROM_BADGES + 5
-	contest_mon  5, VENOMOTH,    LEVEL_FROM_BADGES + 0,  LEVEL_FROM_BADGES + 3
-	contest_mon  5, YANMA,       LEVEL_FROM_BADGES + 1,  LEVEL_FROM_BADGES + 2
-	contest_mon  5, SCYTHER,     LEVEL_FROM_BADGES + 1,  LEVEL_FROM_BADGES + 2
-	contest_mon  5, PINSIR,      LEVEL_FROM_BADGES + 1,  LEVEL_FROM_BADGES + 2
+	;            %, species,     min, max
+	contest_mon 15, CATERPIE,      7,  18
+	contest_mon 15, WEEDLE,        7,  18
+	contest_mon 10, METAPOD,       9,  18
+	contest_mon 10, KAKUNA,        9,  18
+	contest_mon  5, BUTTERFREE,   12,  15
+	contest_mon  5, BEEDRILL,     12,  15
+	contest_mon 10, VENONAT,      10,  16
+	contest_mon 10, PARAS,        10,  17
+	contest_mon  5, VENOMOTH,     12,  15
+	contest_mon  5, YANMA,        13,  14
+	contest_mon  5, SCYTHER,      13,  14
+	contest_mon  5, PINSIR,       13,  14
 ContestMonsEnd:
 
 ; Bug Catching Contest after E4
